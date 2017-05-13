01d922bf941893fc522b6cddc7fb7e8f6497854d
LuffyKing <aderinwale17@gmail.com>
2017-05-13 21:04:20 +0100

    Changed the HTML for the site.

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -55,8 +54,8 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
         'APP_DIRS': True,
+        'DIRS': [os.path.join(BASE_DIR, 'blog/templates')],
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
